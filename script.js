96a13f25acde68a3c6b1c08b14d41f9686287efe
soham <sohamkadam16102004@gmail.com>
2024-09-07 17:19:48 +0530

    This is my second commit

--- script.js
+++ script.js
@@ -4,15 +4,12 @@
         if(clickedButton.classList.contains('active')){
             clickedButton.classList.remove('active');      
         }
-    else
-        {
+        else{
           // Remove 'active' class from all buttons
           buttons.forEach(button => button.classList.remove('active'));
-        
           // Add 'active' class to the clicked button
           clickedButton.classList.add('active');        
-    }
-  
+        }
     }
 
     // Add event listeners to BHK buttons
@@ -27,26 +24,7 @@
         button.addEventListener('click', () => toggleActive(bathroomButtons, button));
     });
 
-    // //Add event listeners to deselect the button
-    // const bhkButton = document.querySelectorAll('.bhk-btn');
-    // bhkButton.forEach(button => {
-    //     button.addEventListener('click', () => removeButton(button,bhkButton))
-    // });
 
-    // const bathroomButton = document.querySelectorAll('.bathrooms-btn');
-    // bathroomButton.forEach(button => { 
-    //     button.addEventListener('click', () => removeButton(button,bathroomButton))
-    // });
-
-
-    // function removeButton(deselectedButton,buttons) {
-    //     //remove 'active class from from deselected button
-    //     if(deselectedButton.classList===buttons.classList)
-    //     {
-    //         deselectedButton.classList.remove('active');
-    //     }
-        
-    // }
     // console.log("Script loaded successfully");
 
 
